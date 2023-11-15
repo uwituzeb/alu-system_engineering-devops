3bf6be499050b7fc1e6fe3c32dbeb7d48fd56a87
bernice uwituze <133651805+uwituzeb@users.noreply.github.com>
2023-11-15 15:19:46 +0200

    fix errors

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # No PasswordAuthentication using puppet
 # SSH configuration file
 file { '/etc/ssh/ssh_config':
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "
-     Host *
-	IdentifyFile ~/.ssh/school
-	PasswordAuthentication no
-     ",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
